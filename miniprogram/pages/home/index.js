4634a74e329995872e4205c67edb6319505d2247
liuhongjia <liuhongjia@lechun.cc>
2026-08-23 13:58:06 +0800

    feat: remove Starline intro banner from home

--- miniprogram/pages/home/index.js
+++ miniprogram/pages/home/index.js
@@ -266,9 +266,6 @@ Page({
   goLogin() {
     wx.navigateTo({ url: "/pages/login/index" });
   },
-  goStarlineIntro() {
-    wx.navigateTo({ url: "/pages/starline-intro/index" });
-  },
   goOpen() {
     if (!this.data.home) {
       this.goLogin();
