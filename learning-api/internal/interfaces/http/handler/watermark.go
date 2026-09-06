49193b419e8e2268fce63834c660afb7ec273f0a
liuhongjia <liuhongjia@lechun.cc>
2026-09-06 08:41:44 +0800

    fix: prevent tiled watermark overlap

--- learning-api/internal/interfaces/http/handler/watermark.go
+++ learning-api/internal/interfaces/http/handler/watermark.go
@@ -157,12 +157,12 @@ func watermarkPageScript(watermarkText string) string {
   translate
   35 rotate
 	halfWidth neg 1200 sub
-	50
+	100
 	halfWidth 1200 add
   {
     /column exch def
 	  halfHeight neg 1200 sub
-	  30
+	  60
 	  halfHeight 1200 add
     {
       /row exch def
